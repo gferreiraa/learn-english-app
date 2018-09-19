66bfe444b8825667f32901be6f33165bea3978b9
gferreira <gferreiraa@gmail.com>
2018-09-19 00:17:29 -0300

    Created component structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { PainelComponent } from './components/painel/painel.component';
+import { AttemptsComponent } from './components/attempts/attempts.component';
+import { ProgressComponent } from './components/progress/progress.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    PainelComponent,
+    AttemptsComponent,
+    ProgressComponent,
   ],
   imports: [
     BrowserModule
