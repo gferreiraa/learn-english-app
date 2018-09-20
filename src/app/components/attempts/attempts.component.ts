36801af0a2325573ecaef33ac3795fe3cd3da13b
gferreira <gferreiraa@gmail.com>
2018-09-19 23:30:13 -0300

    Modified template for use property binding

--- src/app/components/attempts/attempts.component.ts
+++ src/app/components/attempts/attempts.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./attempts.component.css']
 })
 export class AttemptsComponent implements OnInit {
+  public emptyHeart = './assets/coracao_vazio.png';
+  public heartFull = './assets/coracao_cheio.png';
 
   constructor() { }
 
