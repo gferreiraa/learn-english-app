8d6d4b5891d4b3c233daad58be1cf716cb700f9d
gferreira <gferreiraa@gmail.com>
2018-09-24 00:25:42 -0300

    New hearts instantiated

--- src/app/components/attempts/attempts.component.ts
+++ src/app/components/attempts/attempts.component.ts
@@ -1,4 +1,7 @@
+
 import { Component, OnInit } from '@angular/core';
+
+import { Heart } from './../shared/heart.model';
 
 @Component({
   selector: 'app-attempts',
@@ -8,6 +11,10 @@ import { Component, OnInit } from '@angular/core';
 export class AttemptsComponent implements OnInit {
   public emptyHeart = './assets/coracao_vazio.png';
   public heartFull = './assets/coracao_cheio.png';
+
+  public hearts: Heart[] = [
+    new Heart(true), new Heart(true), new Heart(true)
+  ];
 
   constructor() { }
 
